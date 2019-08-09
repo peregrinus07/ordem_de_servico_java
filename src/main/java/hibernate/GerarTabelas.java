2959ac61d4bd96d6aa996ccc7530a980a0c2d12c
tibe <tiberio@cezar.com>
2019-08-09 19:03:27 -0300

    hibernate tabela cliente ok

--- src/main/java/hibernate/GerarTabelas.java
+++ src/main/java/hibernate/GerarTabelas.java
@@ -1,71 +1,33 @@
 package hibernate;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 public class GerarTabelas {
-	
-	static void teste() {
-		  EntityManagerFactory factory = Persistence.
-                  createEntityManagerFactory("hibernate");
 
-      EntityManager manager = factory.createEntityManager();
-      
-      manager.close();
-      factory.close();
-	}
-
-	static void cadastrarCliente() {
-		TabelaCliente cliente = new TabelaCliente();
-		cliente.setNomeCliente("janete");
-		cliente.setEMailCliente("janete@gmail.com");
-		cliente.setTelefoneCliente("1111111");
-		
-		  EntityManagerFactory factory = Persistence.
-                  createEntityManagerFactory("hibernate");
-
-      EntityManager manager = factory.createEntityManager();
-      
-      manager.getTransaction().begin();        
-      manager.persist(cliente);
-      manager.getTransaction().commit();    
-
-      System.out.println("ID da tarefa: " + cliente.getIdCliente());
-
-	}
-	
-	static void pesquisarCliente() {
- 
-		TabelaCliente cliente = new TabelaCliente();
-		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
-	    EntityManager manager = factory.createEntityManager();
-
-	    cliente  = manager.find(cliente.getClass(), 258);
-
-	    System.out.println(cliente.getNomeCliente());
- 		
-	}
 	
 	public static void main(String[] args) {
- 
-TabelaCliente cliente = new TabelaCliente();
-		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
-	    EntityManager manager = factory.createEntityManager();
 
-	    cliente  = manager.find(cliente.getClass(), 258);
+		TabelaCliente cliente = new TabelaCliente();
+		cliente.setNomeCliente("uia");
+		cliente.setTelefoneCliente("222");
+		cliente.setEMailCliente("testegmail");
+		cliente.setIdCliente(261);
 
-	    cliente.setTelefoneCliente("2222");
-	    
-	    manager.getTransaction().begin();
-        manager.merge(cliente);
-        manager.getTransaction().commit();
-	    
-	    System.out.println(cliente.getNomeCliente()+" telefone: "+cliente.getTelefoneCliente());
+		//TabelaCliente teste = new TabelaCliente();
  
+		List<TabelaCliente> teste = cliente.listarClientes(); 
+		 
 		
+         for (TabelaCliente t : teste) {
+            System.out.println(t.getNomeCliente());
+        }
+ 
+       
 	}
 
 }
--- src/main/java/hibernate/TabelaCliente.java
+++ src/main/java/hibernate/TabelaCliente.java
@@ -1,38 +1,135 @@
 package hibernate;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import java.util.List;
 
+import javax.persistence.*;
 
 /**
  * The persistent class for the tabela_clientes database table.
  * 
  */
 @Entity
-@Table(name="tabela_clientes")
-@NamedQuery(name="TabelaCliente.findAll", query="SELECT t FROM TabelaCliente t")
+@Table(name = "tabela_clientes")
+@NamedQuery(name = "TabelaCliente.findAll", query = "SELECT t FROM TabelaCliente t")
 public class TabelaCliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="id_cliente")
+	@Column(name = "id_cliente")
 	private int idCliente;
 
-	@Column(name="cpf_cnpj_cliente")
+	@Column(name = "cpf_cnpj_cliente")
 	private String cpfCnpjCliente;
 
-	@Column(name="e_mail_cliente")
+	@Column(name = "e_mail_cliente")
 	private String eMailCliente;
 
-	@Column(name="nome_cliente")
+	@Column(name = "nome_cliente")
 	private String nomeCliente;
 
-	@Column(name="telefone_cliente")
+	@Column(name = "telefone_cliente")
 	private String telefoneCliente;
 
 	public TabelaCliente() {
 	}
 
+	static void criarTabela() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
+
+		EntityManager manager = factory.createEntityManager();
+
+		manager.close();
+		factory.close();
+
+	}
+
+
+	public void cadastrarCliente(TabelaCliente cliente) {
+		//TabelaCliente 
+		//cliente = new TabelaCliente();
+		//cliente.setNomeCliente("janete");
+		//cliente.setEMailCliente("janete@gmail.com");
+		//cliente.setTelefoneCliente("1111111");
+		
+		  EntityManagerFactory factory = Persistence.
+                  createEntityManagerFactory("hibernate");
+
+      EntityManager manager = factory.createEntityManager();
+      
+      manager.getTransaction().begin();        
+      manager.persist(cliente);
+      manager.getTransaction().commit();    
+
+      System.out.println("ID da tarefa: " + cliente.getIdCliente());
+
+	}
+	
+
+	static void pesquisarClienteId(TabelaCliente cliente) {
+ 
+		//TabelaCliente 
+		
+		//cliente = new TabelaCliente();
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
+	    EntityManager manager = factory.createEntityManager();
+
+	    cliente  = manager.find(cliente.getClass(), cliente.getIdCliente());
+
+	    System.out.println("Nome: "+cliente.getNomeCliente()+" email: "+cliente.getEMailCliente()+" - telefone: "+cliente.getTelefoneCliente());
+ 		
+	}
+
+
+	
+	public void atualizarCliente(TabelaCliente cliente) {
+
+//TabelaCliente 
+		//cliente = new TabelaCliente();
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("hibernate");
+	    EntityManager manager = factory.createEntityManager();
+
+	    TabelaCliente teste  = manager.find(cliente.getClass(), cliente.getIdCliente());
+
+	    cliente.setIdCliente(teste.getIdCliente());
+	    
+	    manager.getTransaction().begin();
+        manager.merge(cliente);
+        manager.getTransaction().commit();
+        
+        pesquisarClienteId(cliente);
+        
+        System.out.println("id cliente: "+cliente.getTelefoneCliente());
+	    System.out.println(cliente.getNomeCliente()+" telefone: "+cliente.getTelefoneCliente());
+
+		
+	} 
+	
+	public static List<TabelaCliente> listarClientes() {
+
+		TabelaCliente cliente = new TabelaCliente();
+
+		EntityManagerFactory factory = Persistence.
+                createEntityManagerFactory("hibernate");
+            EntityManager manager = factory.createEntityManager();
+
+        //cuidado, use o import javax.persistence.Query
+        Query query = manager
+                .createQuery("SELECT t FROM TabelaCliente t");
+        //query.setParameter("paramFinalizado", true);
+
+        List<TabelaCliente> lista = query.getResultList();
+        
+        manager.close();
+ 
+		return (List<TabelaCliente>) lista;
+
+	}
+
+	
+	
 	public int getIdCliente() {
 		return this.idCliente;
 	}
